a0eae0e0783183cafb5a31e500969b793806ac01
DESKTOP-5SRASK1\User <manhnh.ous@mcredit.com.vn>
2023-05-19 17:40:29 +0700

    guide api

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,6 +2,12 @@ import { Router } from "express";
 import { autoPostOnAccount } from "../controllers/instagramControler";
 
 const router = Router();
+//body api http://localhost:8888/auto-post-on-account
+// const body = {
+//   password: "123aA@123",
+//   username: "HaDN123aA",
+//   caption: "Test caption",
+// };
 router.post("/auto-post-on-account", autoPostOnAccount);
 
 export default router;
